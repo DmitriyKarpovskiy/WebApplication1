667c8697b9705d11952fdddbd4f10cc883487e30
Дмитрий Карповский <dmkarpovskiy@gmail.com>
2019-01-23 19:05:26 +0200

    Initial Commit

---
+++ WebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,35 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using WebApplication1.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplication1.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            AddUsers(context);
+        }
+
+        void AddUsers(WebApplication1.Models.ApplicationDbContext context)
+        {
+            var user = new ApplicationUser { UserName = "user1@email.com" };
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            um.Create(user, "password");
+        }
+    }
+}
--- WebApplication1/Models/IdentityModels.cs
+++ WebApplication1/Models/IdentityModels.cs
@@ -25,6 +25,8 @@ namespace WebApplication1.Models
         {
         }
 
+        public DbSet<ToDo> ToDos { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
